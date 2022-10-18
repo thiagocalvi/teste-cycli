62e00e9d06220979613d1aefcd1e025a7b639a7c
thiagocalvi <thiagohcalvi@gmail.com>
2022-10-18 14:22:52 -0300

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json());
+
 app.all('/', (req, res) => {
     res.send('<h1>Bem vindo! Esse é o meu primeiro deploy!!!!</h1>')
 })
+
+app.get('/json', (req, res)=>{
+    res.json({
+        nome: "Teste",
+        message: "Bem vindo!",
+        status: [
+            "Tudo certo meu chapa",
+            "Que triteza"
+        ]
+    })
+})
+
 app.listen(process.env.PORT || 3000)
